2ee0b609367d4e91d97ab2e6bbe132bf8de02b30
Drew Chase <drew.chase@mardens.com>
2026-07-02 16:01:00 -0400

    feat(quill_service): bootstrap Windows service with windows-service crate

--- crates/quill_service/src/main.rs
+++ crates/quill_service/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::ffi::OsString;
+use windows_service::{Result, define_windows_service, service_dispatcher};
+
+define_windows_service!(ffi_service_main, service_main);
+
+fn service_main(_arguments: Vec<OsString>) {
+
+}
+
+fn main() -> Result<()> {
+    service_dispatcher::start("quill-autoprint-service", ffi_service_main)?;
+    Ok(())
 }
